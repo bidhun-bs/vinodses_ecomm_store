b9a0e12efd73c8823a14a1644c1a636cac370d50
Bidhun B S <54357110+bidhun-bs@users.noreply.github.com>
2026-01-18 12:08:42 +0530

    Update BACKEND_API_URL in constants

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,3 +1,3 @@
 export const Constants = {
-    BACKEND_API_URL: 'http://a5a0029d2571e4f3495ab51500f19e55-1888408116.ap-south-1.elb.amazonaws.com',
+    BACKEND_API_URL: 'http://ab3d579c3ebe64bd283b7ad18e8f634b-1812859717.ap-south-1.elb.amazonaws.com    ',
 };
